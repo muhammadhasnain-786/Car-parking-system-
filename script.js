9220c4eb243f9da2b108e30cf534078060fc0cf6
muhammadhasnain-786 <malikhassnain5522@gmail.com>
2026-06-14 10:32:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert("Parking Management System Loaded");
+body{
+    font-family: Arial, sans-serif;
+    text-align: center;
+    margin-top: 30px;
+}
+
+input{
+    padding: 8px;
+    margin: 5px;
+}
+
+button{
+    padding: 8px 12px;
+    margin: 5px;
+}
+
+table{
+    margin: 20px auto;
+    border-collapse: collapse;
+    width: 70%;
+}
+
+th, td{
+    border: 1px solid black;
+    padding: 10px;
+}
